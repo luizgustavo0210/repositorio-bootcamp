36730edc2f2034877ba87c051f1b61945bd67991
eletrica2019anderson@gmail.com <Administrador@gi02tads28.integrado.int>
2024-10-02 20:06:23 -0300

    construindo array de frutas para estudo

---
+++ frutas.js
@@ -0,0 +1,4 @@
+const frutas= [ "maca", "morango", "melancia", "maracuja", "acerola"];
+console.log("a fruta na posição 1 é",frutas[1]);
+console.log("a fruta na posição 4 é", frutas [4]);
+console.log("meu array tem", frutas.length,"frutas");
